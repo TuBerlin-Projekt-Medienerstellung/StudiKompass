5ca32f8a9a382e0ab31c73b8976b13409c9533e1
Jasmin Heiße <heij07.campus@tu-berlin.de>
2026-05-17 13:38:24 +0200

    modulcards

--- components/modul-card.tsx
+++ components/modul-card.tsx
@@ -1,4 +1,4 @@
-import { ChevronUp, Circle, CircleCheckBig } from 'lucide-react';
+import { ChevronUp, Circle, CircleCheckBig, SquareArrowOutUpRight } from 'lucide-react';
 import { details } from "@/constants";
 import Link from "next/link";
 
@@ -32,14 +32,14 @@ const ModulCard = () => {
                     Im Projekt bearbeiten die Studierenden selbstständig in Gruppenarbeit konkrete Probleme. Ein grundsätzliches Projektthema wird vorgegeben, die Ausgestaltung kann in der Gruppe und unter Absprache mit der gruppenleitenden Person selbständig definiert werden. Fortschritte werden in regelmäßigen Absprachen Betreuern besprochen, die Ergebnisse werden in einer Zwischen- sowie in einer Abschlusspräsentation vor allen Teilnehmenden des Kurses vorgestellt sowie den Betreuern einem Projektbericht niedergelegt.
                 </p>
             </div>
-            <div>
+            <div className='flex justify-between gap-2'>
                 {/** Hier fetchen für die Details, gerade werden dummy daten von constants gefetchtet **/}
                 {details.map((detail) => (
-                    <div>
+                    <div className='bg-[#E3E6EA] flex border-2 rounded-xl w-[296px] items-center p-4 flex-col'>
                         <span>
                             {detail.name}
                         </span>
-                        <p>
+                        <p className='font-bold'>
                             {detail.value}
                         </p>
                     </div>
@@ -47,9 +47,13 @@ const ModulCard = () => {
 
 
             </div>
-            <div>
-                buttons und moses link
-                <Link href="#">
+            <div className='flex rounded-lg gap-2'>
+                <button className='bg-[#8F13FD] text-white px-4 py-2 rounded-lg w-5/6'>
+                    Zum Planer hinzufügen
+                </button>
+                <Link href="#" className='bg-[#C40D1F] text-white w-1/6 px-4 py-2 rounded-lg flex items-center justify-center gap-2'>
+                    zu Moses
+                    <SquareArrowOutUpRight className='justify-self-end'/>
                 </Link>
             </div>
 
