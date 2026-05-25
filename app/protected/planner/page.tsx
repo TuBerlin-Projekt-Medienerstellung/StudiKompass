47295ecacf93adce6835bc70749e105d7efb62d9
Jasmin Heiße <heij07.campus@tu-berlin.de>
2026-05-25 14:35:06 +0200

    enthält Modal und actions.ts für die page planer Custom Modul Modal implemntiert, aber ohne Funktionen

--- app/protected/planner/page.tsx
+++ app/protected/planner/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from 'react'
 import SemesterCard from "@/components/semester-card";
+import ModulCardModal from '@/components/modul-card-modal';
 
+
+  
 const Page = () => {
+
+    const modul = {
+        name: "Web Engineering",
+        leistungspunkte: 5,
+        semester: "WS",
+        modulArt: "Pflichtmodul",
+        beschreibung:
+        "Einführung in moderne Webtechnologien.",
+        examform: "Klausur",
+        arbeitsaufwand: "150 Stunden",
+        link: "https://example.com",
+    };
+
     return (
         <section className="flex flex-col gap-4">
             <div className="flex flex-col gap-2">
@@ -9,6 +27,10 @@ const Page = () => {
                 <p className="opacity-70">Plane dein Studium semesterweise</p>
             </div>
             <SemesterCard/>
+            <ModulCardModal
+            isOpen={true}
+            onClose={() => {}}
+            modul={modul}/>
         </section>
     )
 }
