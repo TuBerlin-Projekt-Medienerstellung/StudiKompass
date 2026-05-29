58b6500b987b1d7cfd2c5ef3feffb6717c14268b
Jasmin Heiße <heij07.campus@tu-berlin.de>
2026-05-30 00:26:27 +0200

    modulcard-modal finished, semester-modul-card dynamisch

--- app/protected/planner/page.tsx
+++ app/protected/planner/page.tsx
@@ -3,22 +3,50 @@
 import React from 'react'
 import SemesterCard from "@/components/semester-card";
 import ModulCardModal from '@/components/modul-card-modal';
+import { useState } from "react";
 
+type ModulInfo = {
+  name: string;
+  leistungspunkte: number;
+  semester: string;
+  modulArt: string;
+  beschreibung: string;
+  examform: string;
+  arbeitsaufwand: string;
+  link: string;
+  dozent: string;
+};
 
+const modules : ModulInfo[] = [
+  {
+    name: "Mathe 1",
+    leistungspunkte: 5,
+    semester: "WiSe",
+    modulArt: "Pflicht",
+    beschreibung: "Lorem ipsum",
+    examform: "Klausur",
+    arbeitsaufwand: "150h",
+    link: "/moses",
+    dozent: "Prof. Müller",
+  },
+  {
+    name: "Programmierung",
+    leistungspunkte: 5,
+    semester: "WiSe",
+    modulArt: "Pflicht",
+    beschreibung: "Lorem ipsum",
+    examform: "Projekt",
+    arbeitsaufwand: "120h",
+    link: "/moses",
+    dozent: "Prof. Maier"
+  },
+];
   
 const Page = () => {
 
-    const modul = {
-        name: "Web Engineering",
-        leistungspunkte: 5,
-        semester: "WS",
-        modulArt: "Pflichtmodul",
-        beschreibung:
-        "Einführung in moderne Webtechnologien.",
-        examform: "Klausur",
-        arbeitsaufwand: "150 Stunden",
-        link: "https://example.com",
-    };
+    
+    const [isOpen, setIsOpen] = useState(false);
+    const [modul, setModul] = useState<ModulInfo | null>(null);
 
     return (
         <section className="flex flex-col gap-4">
@@ -26,11 +54,22 @@ const Page = () => {
                 <h1 className="font-bold text-4xl">Studienplaner</h1>
                 <p className="opacity-70">Plane dein Studium semesterweise</p>
             </div>
-            <SemesterCard/>
+            
+            {modules.map((m) => (
+          <SemesterCard
+            key={m.name}
+            modul={m}
+            onClick={() => {
+              setModul(m);
+              setIsOpen(true);
+            }}
+          />
+          ))}
+            
             <ModulCardModal
-            isOpen={true}
-            onClose={() => {}}
-            modul={modul}/>
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+                modul={modul}/>
         </section>
     )
 }
--- components/semester-modul-card.tsx
+++ components/semester-modul-card.tsx
@@ -1,14 +1,32 @@
+"use client";
+
 import React from 'react'
 import Link from "next/link"
 import {SquareArrowOutUpRight} from 'lucide-react';
+import { modulInfo } from '@/constants';
 
+type modulInfo = {
+    name: string;
+    leistungspunkte: number;
+    semester: string;
+    modulArt: string;
+    beschreibung: string;
+    examform: string;
+    arbeitsaufwand: string;
+    link: string;
+}
 
-const SemesterModulCard = () => {
+type Props = {
+  modul:modulInfo;
+  onClick: () => void;
+};
+
+const SemesterModulCard = ({ modul, onClick }: Props) => {
     return (
         <div className="px-6 py-4 rounded-2xl border-x-4 border-y-2 border-flag-red flex flex-col gap-2">
             <h1 className="font-bold text-lg">Grundlagen der Informatik</h1>
             <div className="flex opacity-80 gap-3 text-sm">
-                <span className="text-flag-red">10 ECTS</span> &bull; <span>WiSe</span>&bull; <Link
+                <span className="text-flag-red">10 ECTS </span> &bull; <span>WiSe</span>&bull; <Link
                 className="flex text-blue-bell"
                 href="#">Moses <SquareArrowOutUpRight/></Link>
             </div>
