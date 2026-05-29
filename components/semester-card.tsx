78918a005c2944cc0a451fe415a13d1cf4fe6493
Jasmin Heiße <heij07.campus@tu-berlin.de>
2026-05-30 00:26:27 +0200

    modulcard-modal finished, semester-modul-card dynamisch

--- components/semester-card.tsx
+++ components/semester-card.tsx
@@ -2,9 +2,25 @@ import React from 'react'
 import SemesterModulCard from "@/components/semester-modul-card";
 import {Plus} from 'lucide-react';
 
-const SemesterCard = () => {
+type modulInfo = {
+    name: string;
+    leistungspunkte: number;
+    semester: string;
+    modulArt: string;
+    beschreibung: string;
+    examform: string;
+    arbeitsaufwand: string;
+    link: string;
+}
+
+type Props = {
+  modul: modulInfo;
+  onClick: () => void;
+};
+
+const SemesterCard = ({modul, onClick }: Props) => {
     return (
-        <div className="border-2 rounded-2xl p-4 gap-4 flex flex-col">
+        <div onClick={onClick} className="border-2 rounded-2xl p-4 gap-4 flex flex-col cursor-pointer">
             <header className="flex justify-between">
                 <div>
                     <h2 className="font-bold text-xl">1. Semester
@@ -17,7 +33,10 @@ const SemesterCard = () => {
                 </div>
             </header>
 
-            <SemesterModulCard/>
+            <SemesterModulCard 
+                modul={modul}
+                onClick={() => {
+                console.log(modul.name);}}/>
 
             <button className="border-2 border-dashed rounded-2xl flex items-center justify-center px-6 py-4">
                 <Plus/> Modul hinzufügen
