4de93ed987eee80fff53e57e6c038d3b6cc6255d
Fallen-debug <anjaglavatska@gmail.com>
2026-06-10 18:36:41 +0200

    readded deleted files

---
+++ dev-with-qr.mjs
@@ -0,0 +1,21 @@
+import {spawn} from "child_process";
+import qrcode from "qrcode-terminal";
+// import os from "os";
+
+// Lokale IP holen
+// const interfaces = os.networkInterfaces();
+// let localIP = "localhost";
+// for (const iface of Object.values(interfaces)) {
+//     for (const alias of iface) {
+//         if (alias.family === "IPv4" && !alias.internal) {
+//             localIP = alias.address;
+//         }
+//     }
+// }
+
+const url = `https://studi-kompass-eta.vercel.app`;
+console.log(`\n🔗 Network: ${url}\n`);
+qrcode.generate(url, {small: true});
+
+// Next.js dev starten
+spawn("next", ["dev"], {stdio: "inherit", shell: true});
