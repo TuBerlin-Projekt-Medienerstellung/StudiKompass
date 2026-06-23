014787743eaa9925f86d70dae88aba651eaf18e0
Lena Bohlje <lena.bohlje@campus.tu-berlin.de>
2026-06-23 10:55:32 +0200

    Button für Anzahl Fehlversuche hinzugefügt

--- components/modul-card-modal.tsx
+++ components/modul-card-modal.tsx
@@ -2,11 +2,13 @@
 
 import Link from "next/link";
 import {useState, useEffect} from 'react';
-import { X , SquareArrowOutUpRight, Circle, CircleCheckBig,} from 'lucide-react';
-import { link } from "fs";
-import { details } from "@/constants";
+import {X, SquareArrowOutUpRight, Circle, CircleCheckBig,} from 'lucide-react';
+import {link} from "fs";
+import {details} from "@/constants";
 
-{/**wahrscheinlich neuer type notwendig um alle Infos aus Supabase zu holen, momentan noch modulInfo aus types.d.ts */}
+{/**wahrscheinlich neuer type notwendig um alle Infos aus Supabase zu holen, momentan noch modulInfo aus types.d.ts */
+}
+
 interface ModulInfoSupabase {
     modul_id: number;
     name: string;
@@ -25,37 +27,53 @@ interface ModulInfoSupabase {
 }
 
 type Props = {
-  isOpen: boolean;
-  onClose: () => void;
-  modul: modulInfo | null;
+    isOpen: boolean;
+    onClose: () => void;
+    modul: modulInfo | null;
 };
 
 const ModulCardModal = ({
-  isOpen,
-  onClose,
-  modul,
-}: Props) => {
+                            isOpen,
+                            onClose,
+                            modul,
+                        }: Props) => {
 
-  const [checked, setChecked] = useState(false);
+    const [checked, setChecked] = useState(false);
+    const [counter, setCount] = useState(0);
 
-  useEffect(() => {
-    setChecked(false);
-  }, [modul]);
+    const increase = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        if (counter < 4) {
+            setCount(prev => prev + 1);
+        }
+    };
 
-  if (!modul || !isOpen) return null;
+    const decrease = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        if (counter > 0) {
+            setCount(prev => prev - 1);
+        }
+    };
 
-  {/** Eigentlich noch notwendig: Dozent, pruefungsform (nicht in modulInfo -> neuer type?) */}
-  const {
-    modul_id,
-    name,
-    leistungspunkte,
-    semester,
-    modulArt,
-    link,
-    beschreibung,
 
-  } = modul;
+    useEffect(() => {
+        setChecked(false);
+    }, [modul]);
 
+    if (!modul || !isOpen) return null;
+
+    {/** Eigentlich noch notwendig: Dozent, pruefungsform (nicht in modulInfo -> neuer type?) */
+    }
+    const {
+        modul_id,
+        name,
+        leistungspunkte,
+        semester,
+        modulArt,
+        link,
+        beschreibung,
+
+    } = modul;
 
 
     return (
@@ -63,103 +81,118 @@ const ModulCardModal = ({
         <div>
             {/**wird nur ausgeführt wenn modul =! NULL */}
             {modul && (
-        <div className={`fixed z-50 bg-black/50 rounded-t-xl bottom-0 left-0 md:left-72 right-0 flex justify-center transition-transform transform duration-300 ease-out ${
-        isOpen ? "translate-y-0" : "translate-y-full pointer-events-none"
-        }`}>
-            <div
-            className="w-full bg-white flex flex-col p-8 gap-6 border-y-2 border-x-2 rounded-t-xl">
-                <header className="flex flex-col gap-y-2">
-                    <div className="flex flex-row items-center justify-between">
-                        <div className="flex justifycenter bg-flag-red/50 text-flag-red rounded-xl py-1 px-2 w-fit items-center justify-center">
-                        {modulArt}
-                        </div>
-                        
-                        <button type="button" onClick={onClose}>
-                            <X className="w-4 h-4" />
-                        </button>
+                <div
+                    className={`fixed z-50 bg-black/50 rounded-t-xl bottom-0 left-0 md:left-72 right-0 flex justify-center transition-transform transform duration-300 ease-out ${
+                        isOpen ? "translate-y-0" : "translate-y-full pointer-events-none"
+                    }`}>
+                    <div
+                        className="w-full bg-white flex flex-col p-8 gap-6 border-y-2 border-x-2 rounded-t-xl">
+                        <header className="flex flex-col gap-y-2">
+                            <div className="flex flex-row items-center justify-between">
+                                <div
+                                    className="flex justifycenter bg-flag-red/50 text-flag-red rounded-xl py-1 px-2 w-fit items-center justify-center">
+                                    {modulArt}
+                                </div>
 
+                                <button type="button" onClick={onClose}>
+                                    <X className="w-4 h-4"/>
+                                </button>
+
+                            </div>
+
+                            <h1 className="font-bold md:text-2xl text-xl">
+                                {modul.name}
+                            </h1>
+                            <div className="flex flex-row justify-items-start gap-4">
+                                <p className="text-flag-red">
+                                    {leistungspunkte} ECTS
+                                </p>
+                                <div>•</div>
+                                <p>
+                                    {modul.semester}
+                                </p>
+                                <div>•</div>
+                                {/* Counter für Fehlversuche */}
+                                <p className="text-flag-red">Versuche:</p>
+                                <div
+                                    className="flex items-center gap-1 pl-2 pr-2 text-xs bg-primary-foreground border rounded-xl p-0.5">
+                                    <button
+                                        className="flex items-center justify-center font-bold rounded-lg text-card-foreground hover:opacity-80 transition-all ml-1"
+                                        onClick={decrease}
+                                    >-
+                                    </button>
+                                    <span className="font-semibold text-center text-card-foreground">{counter}</span>
+                                    <button
+                                        className="flex items-center justify-center font-bold rounded-lg text-card-foreground hover:opacity-80 transition-all"
+                                        onClick={increase}
+                                    >+
+                                    </button>
+                                </div>
+                            </div>
+                        </header>
+                        <div>
+                            <h2 className="font-semibold text-lg">
+                                Beschreibung
+                            </h2>
+                            <p>
+                                {modul.beschreibung}
+                            </p>
+                        </div>
+                        <div className="flex flex-col justify-between gap-2 md:flex-row">
+                            <div className="bg-stone-grey flex border-2 rounded-xl w-full items-center p-4 flex-col">
+                                <p>
+                                    Arbeitsaufwand
+                                </p>
+                                <p className="font-semibold">
+                                    {modul.modulArt}
+                                </p>
+                            </div>
+                            <div className="bg-stone-grey flex border-2 rounded-xl w-full items-center p-4 flex-col">
+                                <p>
+                                    Angebot
+                                </p>
+                                <p className="font-semibold">
+                                    {modul.semester}
+                                </p>
+
+                            </div>
+                            <div className="bg-stone-grey flex border-2 rounded-xl w-full items-center p-4 flex-col">
+                                <p>
+                                    Prüfungsform
+                                </p>
+                                <p className="font-semibold">
+                                    {/**{modul.pruefungsform} */}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col gap-2 rounded-lg">
+                            <div className="flex rounded-lg gap-2 flex-col md:flex-row">
+                                {/**Ob das Modul schon abgeschlossen ist -> speichert checked oder nicht checked aber noch nicht richtig :(*/}
+                                <button
+                                    onClick={() => setChecked(!checked)}
+                                    className={`flex px-4 py-2 rounded-lg md:flex-1 justify-center ${
+                                        checked
+                                            ? 'bg-stone-grey text-black'
+                                            : 'bg-mint-leaf text-white'
+                                    }`}>
+                                    {checked
+                                        ? <span className="flex items-center gap-2"><Circle/>Als abgeschlossen markieren</span>
+                                        : <span className="flex items-center gap-2"><CircleCheckBig/>Als abgeschlossen markiert</span>
+                                    }
+                                </button>
+                                <Link href={modul.link}
+                                      className="bg-flag-red text-white px-4 py-2 rounded-lg flex items-center justify-center gap-2 md:w-auto">
+                                    zu Moses
+                                    <SquareArrowOutUpRight/>
+                                </Link>
+                            </div>
+                            <button className="bg-blue-bell text-white px-4 py-2 rounded-lg w-full">
+                                Modul bewerten
+                            </button>
+                        </div>
                     </div>
-                    
-                    <h1 className="font-bold md:text-2xl text-xl">
-                        {modul.name}
-                    </h1>
-                    <div className="flex flex-row justify-items-start gap-4">
-                        <p className="text-flag-red">
-                            {leistungspunkte} ECTS
-                        </p>
-                        <div>•</div>
-                        <p>
-                            {modul.semester}
-                        </p>
-                        <div>•</div>
-                        <p>
-                            {modul.modulArt}
-                        </p>
-                    </div>
-                </header>
-                <div>
-                    <h2 className="font-semibold text-lg"> 
-                        Beschreibung
-                    </h2>
-                    <p>
-                        {modul.beschreibung}
-                    </p>
+
                 </div>
-                <div className="flex flex-col justify-between gap-2 md:flex-row">
-                    <div className="bg-stone-grey flex border-2 rounded-xl w-full items-center p-4 flex-col">
-                        <p>
-                        Arbeitsaufwand 
-                        </p>
-                        <p className="font-semibold">
-                            {modul.modulArt}
-                        </p>
-                    </div>
-                    <div className="bg-stone-grey flex border-2 rounded-xl w-full items-center p-4 flex-col">
-                        <p>
-                            Angebot
-                        </p>
-                        <p className="font-semibold">
-                            {modul.semester}
-                        </p>
-                        
-                    </div>
-                    <div className="bg-stone-grey flex border-2 rounded-xl w-full items-center p-4 flex-col">
-                        <p>
-                            Prüfungsform
-                        </p>
-                        <p className="font-semibold">
-                            {/**{modul.pruefungsform} */}
-                        </p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2 rounded-lg">
-                    <div className="flex rounded-lg gap-2 flex-col md:flex-row">
-                        {/**Ob das Modul schon abgeschlossen ist -> speichert checked oder nicht checked aber noch nicht richtig :(*/}
-                        <button
-                        onClick={() => setChecked(!checked)}
-                        className={`flex px-4 py-2 rounded-lg md:flex-1 justify-center ${
-                        checked
-                        ? 'bg-stone-grey text-black'
-                        : 'bg-mint-leaf text-white'
-                        }`}>
-                            {checked
-                                ? <span className="flex items-center gap-2"><Circle/>Als abgeschlossen markieren</span>
-                                : <span className="flex items-center gap-2"><CircleCheckBig/>Als abgeschlossen markiert</span>
-                            }
-                        </button>
-                        <Link href={modul.link}
-                            className="bg-flag-red text-white px-4 py-2 rounded-lg flex items-center justify-center gap-2 md:w-auto">
-                            zu Moses
-                            <SquareArrowOutUpRight/>
-                        </Link>
-                    </div>
-                    <button className="bg-blue-bell text-white px-4 py-2 rounded-lg w-full">
-                    Modul bewerten
-                </button>
-                </div>
-            </div>
-            
-        </div>
             )}
         </div>
     )
