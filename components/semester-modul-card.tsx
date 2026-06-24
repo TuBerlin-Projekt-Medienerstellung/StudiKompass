01838a5bc8951f76d77e285b168eca893218d25e
Emma <emma_weiland@campus.tu-berlin.de>
2026-06-24 16:50:36 +0200

    Drap and Drop für den Planer implementiert und dnd-kit installiert

--- components/semester-modul-card.tsx
+++ components/semester-modul-card.tsx
@@ -4,6 +4,8 @@ import Link from "next/link"
 import {SquareArrowOutUpRight, Grip, Trash2} from 'lucide-react';
 import ModulCardModal from '@/components/modul-card-modal';
 import {useState} from "react";
+import {useSortable} from "@dnd-kit/sortable";
+import {CSS} from "@dnd-kit/utilities";
 
 type Props = {
     modul: modulInfo;
@@ -13,25 +15,55 @@ type Props = {
 const SemesterModulCard = ({modul, onClick}: Props) => {
 
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedModul] = useState<modulInfo | null>(null);
+    const [selectedModul, setSelectedModul] = useState<modulInfo | null>(null);
+    {/*const valueDisplay = document.getElementById("counter-value"); */}
 
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+        isDragging
+    } = useSortable({ id: String(modul.modul_id) });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0.5 : 1,
+        
+    };
+    
     return (
-        <div>
-            <div onClick={onClick}
-                 className="px-6 py-4 rounded-2xl border-x-4 border-y-2 border-flag-red flex flex-row gap-2 cursor-pointer bg-card">
-                <div className='pr-2 py-4'>
+        <div ref={setNodeRef} style={style}>
+            <div 
+                onClick={onClick}
+                className="px-6 py-4 rounded-2xl border-x-4 border-y-2 border-flag-red flex flex-row gap-2 cursor-pointer bg-card">
+                <div 
+                    {...attributes}
+                    {...listeners}
+                    className='pr-2 py-4'>
+
                     <Grip/>
                 </div>
+
                 <div className='flex flex-col flex-1'>
                     <h1 className="font-bold text-lg">{modul.name}</h1>
                     <div className="flex flex-wrap items-center opacity-80 gap-3 text-sm">
                         <span className="text-flag-red">{modul.leistungspunkte} ECTS</span>
                         <span>WiSe</span>
-                        <Link className="flex items-center gap-1 text-blue-bell" href="#">
+                        <Link 
+                            href="#" 
+                            className="flex items-center gap-1 text-blue-bell" 
+                            onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+                                e.stopPropagation();
+                            }}
+                        >
                             Moses <SquareArrowOutUpRight size={14}/>
                         </Link>
                     </div>
                 </div>
+
                 <button
                     className="flex pr-2 py-4 ml-auto hover:text-flag-red transition-colors"
                     onClick={(e) => {
