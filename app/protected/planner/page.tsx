9042ad6a0f2335789e6b6fc97b5c9307e11a41e3
Alessio_Beu <ale.githubthing@proton.me>
2026-06-27 22:16:39 +0200

    feat(planner): Credits Easter Egg

--- app/protected/planner/page.tsx
+++ app/protected/planner/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SemesterCard from "@/components/semester-card";
+import PlannerTitleEasterEgg from "@/components/planner-title-easter-egg";
 
 const Page = () => {
     return (
         <section className="flex flex-col gap-4">
             <div className="flex flex-col gap-2">
-                <h1 className="font-bold text-4xl">Studienplaner</h1>
+                <PlannerTitleEasterEgg />
                 <p className="text-muted-foreground">Plane dein Studium semesterweise</p>
             </div>
             <SemesterCard/>
